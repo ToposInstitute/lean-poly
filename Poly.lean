07278ca9f348d12b811b64e03d00d05ede7c4db8
David Spivak <dspivak@gmail.com>
2023-09-07 16:21:23 -0700

    poly maps

--- Poly.lean
+++ Poly.lean
@@ -2,27 +2,44 @@ structure Poly where
   pos : Type
   dir : pos -> Type
 
-def constPoly : Type -> Poly := 
-  λ T ↦ 
+def constant (T : Type) : Poly := 
   {
     pos := T
     dir := λ _ ↦ Empty
   }
 
-def one : Poly := constPoly Unit
+def one : Poly := constant Unit
 
-def zero : Poly := constPoly Empty
+def zero : Poly := constant Empty
 
-def linearPoly : Type -> Poly :=
-  λ T ↦ 
+def linear (T : Type) : Poly :=
   {
     pos := T
     dir :=λ _ ↦ Unit
     }
 
-def monomial : Type -> Type -> Poly :=
-  λ P D ↦ 
+def yon : Poly := linear Unit
+
+def monomial (P D : Type) : Poly :=
   {
     pos := P
     dir := λ _ ↦ D 
   }
+
+def representable : Type -> Poly :=
+  λ D ↦
+  {
+    pos := Unit
+    dir := λ _ ↦ D
+  }
+
+----- Maps
+
+def poly_map (P Q: Poly) : Type :=
+  Σ (onPos : P.pos -> Q.pos), 
+  (p : P.pos) -> Q.dir (onPos p) -> P.dir p
+
+def constantMap {T T' : Type} : 
+  (T -> T') -> poly_map (constant T) (constant T') :=
+  λ f ↦ (sigma.mk f absurd) 
+

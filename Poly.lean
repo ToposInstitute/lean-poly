4ac7343fb1a5dc47f60451587330b2555deb4eb1
David Spivak <dspivak@gmail.com>
2023-09-08 16:57:32 -0700

    poly maps as transformations

--- Poly.lean
+++ Poly.lean
@@ -8,8 +8,10 @@ def monomial (P D : Type) : Poly :=
     dir := λ _ ↦ D 
   }
 
-def const (P : Type) : Poly := monomial P Empty
-def linear (P : Type) : Poly := monomial P Unit
+notation:50 A:50 " y^" B:50 => monomial A B  
+
+def const (P : Type) : Poly := P y^Empty  -- monomial P Empty
+def linear (P : Type) : Poly := P y^Unit
 def poly1 : Poly := const Unit
 def poly0 : Poly := const Empty
 def yon : Poly := linear Unit
@@ -19,32 +21,70 @@ def representable (D : Type) : Poly := monomial Unit D
 def bang1 {T : Type} : T -> Unit := λ _ ↦ Unit.unit 
 def ident {T : Type} : T -> T := λ t ↦ t
 
+def applyFun : Poly -> Type -> Type:= 
+  λ p T ↦ Σ (P : p.pos), (p.dir P -> T)
+
 ------------- Maps ------------
 
 def polymap (p q: Poly) : Type :=
   Σ (onPos : p.pos -> q.pos), 
   (P : p.pos) -> q.dir (onPos P) -> p.dir P
 
-def constantMap {T T' : Type} : 
-  (T -> T') -> polymap (const T) (const T') :=
-  λ f ↦ (Sigma.mk f λ _ ↦ Empty.rec ) --I don't know how to map out of empty type
+notation:50 p "⇒" q => polymap p q 
 
-def linearMap {T T' : Type} : (T -> T') -> polymap (linear T) (linear T') :=
+def polymap2 (p q : Poly) : Type :=
+  (P : p.pos) -> Σ (Q : q.pos), q.dir Q -> p.dir P
+
+def cast12 {p q : Poly}: polymap p q -> polymap2 p q :=
+  λ f P ↦
+  (Sigma.mk (f.fst P) (f.snd P))
+
+def cast21 {p q : Poly}: polymap2 p q -> polymap p q :=
+  λ f ↦ 
+  (Sigma.mk (λ P ↦ (f P).fst) (λ P ↦ (f P).snd))
+
+def constantMap {T T' : Type} : 
+  (T -> T') -> (const T) ⇒ (const T') :=
+  λ f ↦ (Sigma.mk f λ _ ↦ Empty.rec ) 
+
+def linearMap {T T' : Type} : (T -> T') ->  (linear T) ⇒ (linear T') :=
   λ f ↦ (Sigma.mk f λ _ _ ↦ Unit.unit)
 
 def representableMap {T T' : Type} : (T -> T') -> polymap (representable T') (representable T) :=
   λ f ↦ (Sigma.mk bang1 λ _ ↦ f)
 
-def polyid {p : Poly} : polymap p p :=
+def polyid {p : Poly} : p ⇒ p :=
   (Sigma.mk (ident) λ _ ↦ ident)
 
-def bang0poly {p : Poly} : polymap poly0 p  := 
+def bang0poly {p : Poly} : poly0 ⇒ p  := 
   (Sigma.mk Empty.rec Empty.rec) 
-def bang1poly {P : Poly} : polymap P poly1 := (Sigma.mk bang1 λ _ ↦ Empty.rec)
 
-def composemap {p q r : Poly} : (polymap p q) -> (polymap q r) -> (polymap p r) :=
+def bang1poly {P : Poly} : P ⇒ poly1 := (Sigma.mk bang1 λ _ ↦ Empty.rec)
+
+def composemap {p q r : Poly} : (p ⇒ q) -> (q ⇒ r) -> (p ⇒ r) :=
   λ f g ↦ 
   let onPos : p.pos -> r.pos := g.fst ∘ f.fst
   let onDir : (P : p.pos) -> (r.dir $ onPos P) -> (p.dir P) := λ P rd ↦ (f.snd P $ g.snd (f.fst P) rd)
   (Sigma.mk onPos onDir)
 
+notation:50 f:50 ";" g:50  => composemap f g
+
+theorem assoc {p q r s : Poly} : 
+  (f : p ⇒ q) -> (g : q ⇒ r) -> (h : r ⇒ s) -> 
+  (f ; (g ; h)) = ((f ; g) ; h) := by
+  intros
+  rfl
+
+theorem unitl {p q : Poly} : (f : p ⇒ q) -> f = (f ; polyid) := by
+  intros
+  rfl
+
+theorem unitr {p q : Poly} : (f : p ⇒ q) -> f = (polyid ; f) := by
+  intros
+  rfl
+
+def toTransformation {p q : Poly} : (f : p ⇒ q) -> (T : Type) -> (applyFun p T) -> (applyFun q T) := 
+  λ f T Pt ↦ 
+  let P := Pt.fst
+  let t := Pt.snd P
+  (Sigma.mk (f.fst P) ?)
